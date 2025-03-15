a8935653f10bc7ae1a5ad5e52976a968c50be167
Hyuk <testerhyuk214@gmail.com>
2025-03-15 23:20:30 +0900

    Comment CRUD, 인덱스, 페이징 및 최적화 완료

---
+++ livemarket_backend/service/comment/src/main/java/comment/service/CommentService.java
@@ -0,0 +1,103 @@
+package comment.service;
+
+import comment.entity.Comment;
+import comment.repository.CommentRepository;
+import comment.service.request.CommentCreateRequest;
+import comment.service.response.CommentPageResponse;
+import comment.service.response.CommentResponse;
+import livemarket.backend.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.function.Predicate.not;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final Snowflake snowflake = new Snowflake();
+
+    @Transactional
+    public CommentResponse create(CommentCreateRequest request) {
+        Comment parent = findParent(request);
+
+        Comment comment = commentRepository.save(
+                Comment.create(
+                        snowflake.nextId(),
+                        request.getContent(),
+                        parent == null ? null : parent.getCommentId(),
+                        request.getArticleId(),
+                        request.getWriterId()
+                )
+        );
+
+        return CommentResponse.from(comment);
+    }
+
+    private Comment findParent(CommentCreateRequest request) {
+        Long parentCommentId = request.getParentCommentId();
+
+        if (parentCommentId == null) {
+            return null;
+        }
+
+        return commentRepository.findById(parentCommentId)
+                .filter(not(Comment::getDeleted))
+                .filter(Comment::isRoot)
+                .orElseThrow();
+    }
+
+    public CommentResponse read(Long commentId) {
+        return CommentResponse.from(commentRepository.findById(commentId).orElseThrow());
+    }
+
+    @Transactional
+    public void delete(Long commentId) {
+        commentRepository.findById(commentId)
+                .filter(not(Comment::getDeleted))
+                .ifPresent(comment -> {
+                    if (hasChildren(comment)) {
+                        comment.delete();
+                    } else {
+                        delete(comment);
+                    }
+                });
+    }
+
+    private boolean hasChildren(Comment comment) {
+        return commentRepository.countBy(comment.getArticleId(), comment.getCommentId(), 2L) == 2;
+    }
+
+    private void delete(Comment comment) {
+        commentRepository.delete(comment);
+
+        if(!comment.isRoot()) {
+            commentRepository.findById(comment.getParentCommentId())
+                    .filter(Comment::getDeleted)
+                    .filter(not(this::hasChildren))
+                    .ifPresent(this::delete);
+        }
+    }
+
+    public CommentPageResponse readAll(Long articleId, Long page, Long pageSize) {
+        return CommentPageResponse.of(
+                commentRepository.findAll(articleId, (page - 1) * pageSize, pageSize).stream()
+                        .map(CommentResponse::from)
+                        .toList(),
+                commentRepository.count(articleId, PageLimitCalculator.calculatePageLimit(page, pageSize, 10L))
+        );
+    }
+
+    // 무한 스크롤
+    public List<CommentResponse> readAll(Long articleId, Long lastParentCommentId, Long lastCommentId, Long limit) {
+        List<Comment> comments = lastParentCommentId == null || lastCommentId == null ?
+                commentRepository.findAllInfiniteScroll(articleId, limit) :
+                commentRepository.findAllInfiniteScroll(articleId, lastParentCommentId, lastCommentId, limit);
+        return comments.stream()
+                .map(CommentResponse::from)
+                .toList();
+    }
+}
